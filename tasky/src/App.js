730c90968e89ec43ee29b5679a1f30b08dd53b4d
belphy <20101931@wit.ie>
2024-10-12 23:13:17 +0100

    Added keys to tasks

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -6,22 +6,24 @@ function App() {
 
   const [ taskState, setTaskState ] = useState({
     tasks: [
-      { title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { title: "Tidy up", deadline: "Today" }
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { id: 3, title: "Tidy up", deadline: "Today" }
     ]
   });
   
   return (
     <div className="container">
       <h1>Tasky</h1>
+     
       {taskState.tasks.map((task) => (              
     <Task 
       title={task.title}
       description={task.description}
       deadline={task.deadline}
+      key={task.id}
     />
-  ))} 
+  ))}
     </div>
   );
 }
