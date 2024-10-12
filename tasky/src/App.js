c17cf5cbc372cfcb881813121e0c07cf7ac3a27d
belphy <20101931@wit.ie>
2024-10-12 23:18:40 +0100

    Delete a task

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -3,27 +3,40 @@ import Task from './components/Task';
 import React, { useState } from 'react';
 
 function App() {
-
-  const [ taskState, setTaskState ] = useState({
+  const [taskState, setTaskState] = useState({
     tasks: [
-      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
-      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
-      { id: 3, title: "Tidy up", deadline: "Today" }
+      { id: 1, title: "Dishes", description: "Empty dishwasher", deadline: "Today", done: false },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow", done: false },
+      { id: 3, title: "Tidy up", description: "Tidy up the living room", deadline: "Today", done: false }
     ]
   });
-  
+
+  const doneHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks[taskIndex].done = !tasks[taskIndex].done;
+    setTaskState({ tasks });
+  };
+
+  const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({ tasks });
+  };
+
   return (
     <div className="container">
       <h1>Tasky</h1>
-     
-      {taskState.tasks.map((task) => (              
-    <Task 
-      title={task.title}
-      description={task.description}
-      deadline={task.deadline}
-      key={task.id}
-    />
-  ))}
+      {taskState.tasks.map((task, index) => (
+        <Task
+          key={task.id}
+          title={task.title}
+          description={task.description}
+          deadline={task.deadline}
+          done={task.done}
+          markDone={() => doneHandler(index)}
+          deleteTask={() => deleteHandler(index)}
+        />
+      ))}
     </div>
   );
 }
